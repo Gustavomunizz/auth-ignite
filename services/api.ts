b4cca53e727f97d795b1ea939bc1a86a10a4135f
Gustavo Muniz Martins <gustamunizmartins@gmail.com>
2022-10-29 18:04:44 -0300

    Fazendo logout automático

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError } from 'axios'
 import { parseCookies, setCookie } from 'nookies'
+import { signOut } from '../contexts/authContext'
 
 interface AxiosErrorResponse {
   code?: string
@@ -77,9 +78,10 @@ api.interceptors.response.use(
           })
         })
       } else {
-        // deslogar usuário
+        signOut()
       }
     }
+    return Promise.reject(error)
   }
 )
 
